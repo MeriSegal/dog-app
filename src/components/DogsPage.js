062b91c37355f218a09f5f9da4965d6b5dae8bad
MeriSg <sgmerisg@gmail.com>
2020-08-13 20:29:40 +0300

    add update button and design dogs page

--- src/components/DogsPage.js
+++ src/components/DogsPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import {Container } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import DogsModel from '../data-model/DogsModel';
@@ -11,7 +11,8 @@ class DogsPage extends React.Component {
         super(props);
 
         this.state = {
-            race: "",           
+            race: "", 
+            raceList: [],          
             dogsData: [], 
             id: -1   
         }
@@ -25,23 +26,31 @@ class DogsPage extends React.Component {
 
 
 
-    getUrl = (race) =>{
+    getUrl = (race, isUpdate) =>{
         axios.get(`https://dog.ceo/api/breed/${race}/images/random`).then(response => {
+            isUpdate ?
+            this.state.dogsData.find(dog => dog.raceName === race).imgUrl = response.data.message:
             this.state.dogsData.push(new DogsModel(race,response.data.message))
-            this.setState({
+            this.setState({               
                 dogsData: this.state.dogsData
             }); 
         })
       }
      
+    updateUrl =()=>{
+         this.state.raceList.map(race => this.getUrl(race, true))
+    }
 
     componentDidMount() {
 
         const URL = "https://dog.ceo/api/breeds/list"
-        axios.get(URL).then(response => {          
+        axios.get(URL).then(response => {  
+            this.setState({
+                raceList: response.data.message
+            })
             response.data.message.map(race => 
-                this.getUrl(race))
-          })      
+                this.getUrl(race, false))                
+            })      
 
       }
 
@@ -53,12 +62,12 @@ class DogsPage extends React.Component {
 
         const contentToRender= dogs.filter(dog => (dog.raceName).toLowerCase().includes((this.state.race).toLowerCase())).map((filterDog, index) =>
       
-           <div key={index}  className="col-xl-4">
+           <div key={index} className="col-xl-3 col-md-4 dogCard">
                 <div className="card">
-                <div className="card-body">
-                    <p className="card-text">{filterDog.raceName} </p>
-                </div> 
-                <img className="card-img-bottom" src={filterDog.imgUrl} alt={filterDog.raceName}/>
+                    <div className="card-body">
+                        <p className="card-text">{filterDog.raceName} </p>
+                    </div> 
+                    <img className="card-img-bottom" src={filterDog.imgUrl} alt={filterDog.raceName}/>
                 </div>                
             </div>
             );
@@ -66,7 +75,15 @@ class DogsPage extends React.Component {
 
         return(
             <div className="wrap">
-                <input type="search" id="race" placeholder="" onChange={this.updateRace} value={this.state.race}/>
+                <div className="up-box">
+                    <div className="l-box">
+                        <lable for="race" >Search </lable>
+                        <input  type="search" id="race" placeholder="" onChange={this.updateRace} value={this.state.race}/>
+                    </div>
+                    <div className="r-box">
+                        <button type="button" onClick={this.updateUrl}>Update Images</button>
+                    </div>
+                </div> 
 
                 <Container>
                     <div className="row">
